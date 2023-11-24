9f723c088f295af59a09b045ec4a6323c3876c12
staslif <149203375+staslif@users.noreply.github.com>
2023-11-24 11:16:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("введите x");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите y");
+int y = Convert.ToInt32(Console.ReadLine());
+try
+{
+    int z = x / y;
+    Console.WriteLine(z);
+}
+catch
+{
+    Console.WriteLine("исключение:");
+}
+try
+{
+    int z = x / y;
+    if (x == 0)
+    {
+        throw new Exception("результат в переменной z будет нулевым");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
